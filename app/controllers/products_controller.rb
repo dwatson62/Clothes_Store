1189166022e245c2ef83e4b5806b637e04264172
Daryl Watson <darylwtsn@gmail.com>
2015-07-25 16:02:40 +0100

    Can remove an item from the shopping cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,18 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     product.quantity -= 1
     product.save
-    order = Order.find(params[:id])
+    order = Order.find(params[:order_id])
     order.products << product
+    redirect_to '/'
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.quantity += 1
+    product.save
+    order = Order.find(params[:order_id])
+    index = order.products.find(product.id)
+    order.products.delete(product)
     redirect_to '/'
   end
 
--- spec/features/orders_feature_spec.rb
+++ spec/features/orders_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'A user on the home page' do
+feature 'A user arrives on the home page for the first time' do
 
   before(:each) do
     Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 1)
@@ -16,9 +16,33 @@ feature 'A user on the home page' do
   end
 
   scenario 'can add an item to the shopping cart' do
+    order = Order.first
     click_on 'Add to cart'
-    expect(page).to have_content 'Black Shoes'
+    expect(page).to have_content 'Shopping cart: Black Shoes'
     expect(page).not_to have_content 'Empty!'
+    expect(order.products.length).to eq 1
+  end
+
+end
+
+feature 'A user while shopping' do
+
+  before(:each) do
+    product = Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 1)
+    order = Order.create
+    order.products << product
+    visit '/'
+  end
+
+  scenario 'displays current shopping cart' do
+    expect(page).to have_content 'Shopping cart: Black Shoes'
+    expect(page).not_to have_content 'Empty!'
+  end
+
+  scenario 'can remove a product from the cart' do
+    click_on 'Remove item'
+    expect(page).not_to have_content 'Black shoes'
+    expect(page).to have_content 'Empty!'
   end
 
 end

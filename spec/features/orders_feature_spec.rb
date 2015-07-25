d091a1cea5e31e25f3e56560bab7025c6337a7c3
Daryl Watson <darylwtsn@gmail.com>
2015-07-25 16:28:52 +0100

    Displays items as out of stock when quantity reaches 0

--- spec/features/orders_feature_spec.rb
+++ spec/features/orders_feature_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'A user arrives on the home page for the first time' do
 
   before(:each) do
-    Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 1)
+    Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 2)
+    Product.create(name: 'Black Tie', category: "Men's Formalwear", quantity: 0)
     visit '/'
   end
 
@@ -23,20 +24,29 @@ feature 'A user arrives on the home page for the first time' do
     expect(order.products.length).to eq 1
   end
 
+  scenario 'cannot order an item that is out of stock' do
+    expect(page).to have_content "Black Tie - Men's Formalwear Out of Stock"
+  end
+
 end
 
 feature 'A user while shopping' do
 
   before(:each) do
-    product = Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 1, price: 9.99)
-    order = Order.create
-    order.products << product
+    product = Product.create(name: 'Black Shoes', category: "Men's Footwear", quantity: 2, price: 9.99)
     visit '/'
+    click_on 'Add to cart'
   end
 
   scenario 'displays current shopping cart' do
     expect(page).to have_content 'Shopping cart: Black Shoes'
     expect(page).not_to have_content 'Empty!'
+  end
+
+  scenario 'when last of a product is ordered, it is displayed as out of stock' do
+    expect(page).to have_content "Black Shoes - Men's Footwear"
+    click_on 'Add to cart'
+    expect(page).to have_content "Black Shoes - Men's Footwear Out of Stock"
   end
 
   scenario 'can remove a product from the cart' do
